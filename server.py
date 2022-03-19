e4e8a9b4be87d21598020b28ac03d61eeafcf48d
nzyuko <johnnzyuko514@gmail.com>
2022-03-19 12:57:19 +0300

    ip address config specified

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import threading
 import socket
 
-host = '127.0.0.1'  # localhost
+host = '127.0.0.1'  # currently, set to localhost change to the public ip address of the server the script is running on
 port = 55555
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
