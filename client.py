315044553696d248cd9b11e7ff35d88c4aa12279
nzyuko <johnnzyuko514@gmail.com>
2022-03-19 08:38:51 +0300

    receive function checks if the keyword from the server is NICK then sends the nickname to the server

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+import threading
+
+nickname = input("Choose a nickname : ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+
+            else:
+                print(message)
+        except:
+            print("An Error Occured")
+            client.close()
+            break
+
+
